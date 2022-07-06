4f7f5e3b433ef52a0b56650027f6846a7e7384eb
Shubh <shubh.scb@gmail.com>
2022-07-07 00:14:48 +0530

    LINTERS ERR FIX

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import './AddBook.css'
+import './AddBook.css';
 
-const AddBook = () => {
-    return (
-  <div className='input-container'>
+const AddBook = () => (
+  <div className="input-container">
     <div>
-    <h3>Add new Book</h3>
-    <form className='form'>
-      <input type="text" placeholder="Add Book" />
-      <input type="text" placeholder="Author" />
-      <button type="button">Add Book</button>
-    </form>
+      <h3>Add new Book</h3>
+      <form className="form">
+        <input type="text" placeholder="Add Book" />
+        <input type="text" placeholder="Author" />
+        <button type="button">Add Book</button>
+      </form>
     </div>
   </div>
-    )
-};
+);
 
 export default AddBook;
--- src/components/Book.js
+++ src/components/Book.js
@@ -2,18 +2,18 @@ import React from 'react';
 import './Book.css';
 
 const Book = (book) => {
-    let { title, author } = book;
-    title = "Mistress of Spices";
-    author = "Chitra Banerjee Divakaruni"
-return (
-  <div className="book-container">
-    <h3>{title}</h3>
-    <p>{author}</p>
-    <div className='button-container'>
-    <button type='button'>Remove</button>
+  let { title, author } = book;
+  title = 'Mistress of Spices';
+  author = 'Chitra Banerjee Divakaruni';
+  return (
+    <div className="book-container">
+      <h3>{title}</h3>
+      <p>{author}</p>
+      <div className="button-container">
+        <button type="button">Remove</button>
+      </div>
     </div>
-  </div>
-)
+  );
 };
 
 export default Book;
