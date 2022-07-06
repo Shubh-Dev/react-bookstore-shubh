350afa8f8eea71c0f523ebaeeeb12908f96f6f0c
Shubh <shubh.scb@gmail.com>
2022-07-07 00:02:19 +0530

    RECEIVE DATA AS PROPS PROGRESS

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './AddBook.css'
 
 const AddBook = () => (
-  <div>
-    <form>
+  <div className='input-container'>
+    <div>
+    <h3>Add new Book</h3>
+    <form className='form'>
       <input type="text" placeholder="Add Book" />
       <input type="text" placeholder="Author" />
       <button type="button">Add Book</button>
     </form>
-
+    </div>
   </div>
 );
 
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Book.css';
 
-const Book = () => (
+const Book = (book) => {
+    let { title, author } = book;
+    title = "Mistress of Spices";
+    author = "Chitra Banerjee Divakaruni"
+return (
   <div className="book-container">
-    <h3>Book-1</h3>
+    <h3>{title}</h3>
+    <p>{author}</p>
+    <div className='button-container'>
+    <button type='button'>Comments</button>
+    <button type='button'>Remove</button>
+    <button type='button'>Edit</button>
+    </div>
   </div>
-);
+)
+};
 
 export default Book;
