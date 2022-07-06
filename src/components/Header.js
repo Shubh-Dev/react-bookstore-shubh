9ee73fc34bd109914228a1924c761b86dcea2d54
Shubh <shubh.scb@gmail.com>
2022-07-06 19:37:05 +0530

    LINTERS ADD

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+const Header = () => (
+  <div>
+    <h2>Book Store</h2>
+    <nav>
+      <li>BOOKS</li>
+      <li>CATEGORIES</li>
+    </nav>
+  </div>
+);
+
+export default Header;
