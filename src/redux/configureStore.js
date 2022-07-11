cc01b8687ba52c5c638a82308b095ca0fcc421d3
Shubh <shubh.scb@gmail.com>
2022-07-11 19:36:07 +0530

    STORE CONFIG COMPLETE

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,6 +4,10 @@ import bookReducerFunc from './books/books';
 import CategoryReducer from './categories/categories';
 
 const rootReducer = combineReducers({
-    bookReducerFunc
-    CategoryReducer
-})
+   bookReducer: bookReducerFunc,
+   categoryReducer: CategoryReducer
+});
+
+const store = combineReducers(rootReducer);
+
+export default store;
