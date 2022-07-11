507ea43fbe73a2de687b13c541c7ead26a3d4a73
Shubh <shubh.scb@gmail.com>
2022-07-12 00:11:40 +0530

    ADD KEY TO ROOT REDUCER

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,8 +4,8 @@ import bookReducerFunc from './books/books';
 import CategoryReducer from './categories/categories';
 
 const rootReducer = combineReducers({
-  bookReducerFunc,
-  CategoryReducer,
+  bookReducer: bookReducerFunc,
+  categoryReducer: CategoryReducer,
 });
 
 const store = configureStore(rootReducer);
