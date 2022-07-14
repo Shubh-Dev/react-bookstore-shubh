a51750caae251fb4a73ac18b2fd2f640e77edc09
Shubh <shubh.scb@gmail.com>
2022-07-14 23:23:36 +0530

    CIRCULAR PROGRESSBAR ADD

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './scss/Progress.scss';
-// import { CircularProgressbar } from 'react-circular-progressbar';
-// import 'react-circular-progressbar/dist/styles.css';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
-const Progress = () => (
-  <div className="progress-container d_flex_r g10">
-    <div className="circle-wrap">
-      <div className="circle" />
+const Progress = () => {
+  const [percentage, setPercentage] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (percentage < 100) {
+        setPercentage(percentage + 1);
+      }
+    }, 50);
+  }, [percentage]);
+
+  return (
+    <div className="progress-container d_flex_r g10">
+      <div className="circle-wrap">
+        <CircularProgressbar value={percentage} text={`${percentage}%`} />
+      </div>
+      <div className="percentage_container">
+        <h3 className="percent-progress">100%</h3>
+        <p className="complete">Completed</p>
+      </div>
     </div>
-    <div className="percentage_container">
-      <h3 className="percent-progress">100%</h3>
-      <p className="complete">Completed</p>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Progress;
